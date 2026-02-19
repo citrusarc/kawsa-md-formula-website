d65144ec1400104edc0b1b4cdaa0bbca239f1c24
citrusarc <citrusarc.studio@gmail.com>
2026-02-19 12:20:38 +0800

    fix navbar add semantic

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -83,182 +83,185 @@ export default function Navbar() {
   }, []);
 
   return (
-    <nav
-      className={clsx(
-        "sticky top-0 z-50 flex p-4 sm:py-4 sm:px-24 w-full items-center justify-between transition-colors duration-300",
-        // openMenu
-        //   ? "text-black bg-white"
-        // : scroll
-        // ? "text-white bg-violet-600"
-        !(isHome || isWhatCustomersSay)
-          ? "text-black bg-white"
-          : "text-white bg-transparent",
-      )}
-    >
-      <button
-        onClick={() => {
-          setOpenMenu(!openMenu);
-          setOpenCart(false);
-        }}
-        className="cursor-pointer"
-      >
-        {openMenu ? (
-          <Xmark className="w-6 h-6" />
-        ) : (
-          <Menu className="w-6 h-6" />
+    <header className="sticky top-0 z-50">
+      <nav
+        aria-label="Main navigation"
+        className={clsx(
+          "relative flex p-4 sm:py-4 sm:px-24 w-full items-center justify-between transition-colors duration-300",
+          // openMenu
+          //   ? "text-black bg-white"
+          // : scroll
+          // ? "text-white bg-violet-600"
+          !(isHome || isWhatCustomersSay)
+            ? "text-black bg-white"
+            : "text-white bg-transparent",
         )}
-      </button>
-      <Link href="/" className="cursor-pointer">
-        <BrandLogo className="w-16 sm:w-24 h-8 sm:h-12" />
-      </Link>
-      <button
-        onClick={() => {
-          setOpenCart(!openCart);
-          setOpenMenu(false);
-          resetSwipe();
-        }}
-        className="relative cursor-pointer"
       >
-        <ShoppingBag className="w-6 h-6" />
-        <span className="absolute -top-2 -right-3  px-2 py-0.5 text-sm rounded-full text-white bg-violet-600 ">
-          {mounted ? cartCount : 0}
-        </span>
-      </button>
-      {openMenu && (
-        <div
-          ref={menuRef}
-          className={clsx(
-            "absolute top-full left-1/2 -translate-x-1/2 p-4 w-[94vw] max-w-[2400px] h-fit shadow-md rounded-2xl sm:rounded-4xl overflow-hidden backdrop-blur-2xl text-black bg-violet-100/80 transform transition-all duration-300 origin-top",
-            openMenu
-              ? "opacity-100 scale-y-100 pointer-events-auto"
-              : "opacity-0 scale-y-0 pointer-events-none",
-          )}
+        <button
+          onClick={() => {
+            setOpenMenu(!openMenu);
+            setOpenCart(false);
+          }}
+          className="cursor-pointer"
         >
-          <div className="flex flex-col gap-4">
-            {navItems.map((item) => (
-              <Link
-                key={item.id}
-                href={item.href}
-                onClick={() => setOpenMenu(false)}
-                className="p-4 w-fit rounded-2xl overflow-hidden text-xl hover:text-white hover:bg-violet-600"
-              >
-                {item.name}
-              </Link>
-            ))}
+          {openMenu ? (
+            <Xmark className="w-6 h-6" />
+          ) : (
+            <Menu className="w-6 h-6" />
+          )}
+        </button>
+        <Link href="/" className="cursor-pointer">
+          <BrandLogo className="w-16 sm:w-24 h-8 sm:h-12" />
+        </Link>
+        <button
+          onClick={() => {
+            setOpenCart(!openCart);
+            setOpenMenu(false);
+            resetSwipe();
+          }}
+          className="relative cursor-pointer"
+        >
+          <ShoppingBag className="w-6 h-6" />
+          <span className="absolute -top-2 -right-3  px-2 py-0.5 text-sm rounded-full text-white bg-violet-600 ">
+            {mounted ? cartCount : 0}
+          </span>
+        </button>
+        {openMenu && (
+          <div
+            ref={menuRef}
+            className={clsx(
+              "absolute top-full left-1/2 -translate-x-1/2 p-4 w-[94vw] max-w-[2400px] h-fit shadow-md rounded-2xl sm:rounded-4xl overflow-hidden backdrop-blur-2xl text-black bg-violet-100/80 transform transition-all duration-300 origin-top",
+              openMenu
+                ? "opacity-100 scale-y-100 pointer-events-auto"
+                : "opacity-0 scale-y-0 pointer-events-none",
+            )}
+          >
+            <div className="flex flex-col gap-4">
+              {navItems.map((item) => (
+                <Link
+                  key={item.id}
+                  href={item.href}
+                  onClick={() => setOpenMenu(false)}
+                  className="p-4 w-fit rounded-2xl overflow-hidden text-xl hover:text-white hover:bg-violet-600"
+                >
+                  {item.name}
+                </Link>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
+        )}
 
-      {mounted && openCart && (
-        <div
-          ref={cartRef}
-          className={clsx(
-            "absolute top-0 right-0 z-50 p-4 sm:p-8 w-full sm:w-[30vw] h-dvh transform transition-transform duration-300 shadow-md backdrop-blur-2xl text-violet-600 bg-white",
-            openCart ? "translate-x-0" : "translate-x-full",
-          )}
-        >
-          <div className="relative flex flex-col gap-4 sm:gap-8 w-full h-full overflow-hidden">
-            <button
-              onClick={() => setOpenCart(false)}
-              className="absolute top-0 right-0 cursor-pointer"
-            >
-              <Xmark className="w-6 h-6 text-black" />
-            </button>
-            <h2 className="text-xl font-semibold">Your Cart</h2>
-            {cartItems.length === 0 ? (
-              <p className="text-neutral-400">
-                You currently have no items in your cart.
-              </p>
-            ) : (
-              <div className="flex flex-1 flex-col gap-4 sm:gap-8 pb-[calc(env(safe-area-inset-bottom)+12dvh)] overflow-y-auto">
-                {cartItems.map((item) => (
-                  <div key={item.id} className="flex relative">
-                    <div className="absolute flex sm:hidden inset-y-0 right-0 w-[80px] items-center justify-center">
-                      <button
-                        className="flex w-full h-full items-center justify-center text-red-100 bg-red-500"
-                        onClick={() => useCart.getState().clearItem(item.id)}
-                      >
-                        <Trash className="w-6 h-6" />
-                      </button>
-                    </div>
-                    <div className="flex w-full items-center justify-between text-black">
-                      <div
-                        className={clsx(
-                          "relative flex z-10 w-full items-start gap-4 bg-white p-2 transition-transform duration-200",
-                          item.swiped && "sm:translate-x-0 -translate-x-24",
-                        )}
-                        data-swipe-item
-                        onTouchStart={(e) =>
-                          startSwipe(item.id, e.touches[0].clientX)
-                        }
-                        onTouchMove={(e) =>
-                          moveSwipe(item.id, e.touches[0].clientX)
-                        }
-                        onTouchEnd={() => endSwipe(item.id)}
-                      >
-                        <div className="relative shrink-0 w-32 h-32 rounded-xl sm:rounded-2xl overflow-hidden">
-                          <Image
-                            fill
-                            src={item.src}
-                            alt={item.name}
-                            className="object-cover"
-                          />
-                        </div>
-                        <div className="flex flex-col gap-2 justify-between w-full">
-                          <div>
-                            <p className="line-clamp-2 wrap-break-words font-semibold">
-                              {item.name}
-                            </p>
-                            <p className="text-neutral-400">
-                              RM
-                              {(item.currentPrice ?? item.unitPrice) *
-                                item.quantity}
-                            </p>
-                          </div>
-                          <Stepper
-                            value={item.quantity}
-                            min={1}
-                            onChange={(setNewQuantity) =>
-                              useCart
-                                .getState()
-                                .setQuantity(item.id, setNewQuantity)
-                            }
-                          />
-                        </div>
+        {mounted && openCart && (
+          <div
+            ref={cartRef}
+            className={clsx(
+              "absolute top-0 right-0 z-50 p-4 sm:p-8 w-full sm:w-[30vw] h-dvh transform transition-transform duration-300 shadow-md backdrop-blur-2xl text-violet-600 bg-white",
+              openCart ? "translate-x-0" : "translate-x-full",
+            )}
+          >
+            <div className="relative flex flex-col gap-4 sm:gap-8 w-full h-full overflow-hidden">
+              <button
+                onClick={() => setOpenCart(false)}
+                className="absolute top-0 right-0 cursor-pointer"
+              >
+                <Xmark className="w-6 h-6 text-black" />
+              </button>
+              <h2 className="text-xl font-semibold">Your Cart</h2>
+              {cartItems.length === 0 ? (
+                <p className="text-neutral-400">
+                  You currently have no items in your cart.
+                </p>
+              ) : (
+                <div className="flex flex-1 flex-col gap-4 sm:gap-8 pb-[calc(env(safe-area-inset-bottom)+12dvh)] overflow-y-auto">
+                  {cartItems.map((item) => (
+                    <div key={item.id} className="flex relative">
+                      <div className="absolute flex sm:hidden inset-y-0 right-0 w-[80px] items-center justify-center">
+                        <button
+                          className="flex w-full h-full items-center justify-center text-red-100 bg-red-500"
+                          onClick={() => useCart.getState().clearItem(item.id)}
+                        >
+                          <Trash className="w-6 h-6" />
+                        </button>
                       </div>
-                      <button
-                        className="p-3 hidden sm:block shrink-0 cursor-pointer rounded-full text-red-500 hover:bg-red-100"
-                        onClick={() => useCart.getState().clearItem(item.id)}
+                      <div className="flex w-full items-center justify-between text-black">
+                        <div
+                          className={clsx(
+                            "relative flex z-10 w-full items-start gap-4 bg-white p-2 transition-transform duration-200",
+                            item.swiped && "sm:translate-x-0 -translate-x-24",
+                          )}
+                          data-swipe-item
+                          onTouchStart={(e) =>
+                            startSwipe(item.id, e.touches[0].clientX)
+                          }
+                          onTouchMove={(e) =>
+                            moveSwipe(item.id, e.touches[0].clientX)
+                          }
+                          onTouchEnd={() => endSwipe(item.id)}
+                        >
+                          <div className="relative shrink-0 w-32 h-32 rounded-xl sm:rounded-2xl overflow-hidden">
+                            <Image
+                              fill
+                              src={item.src}
+                              alt={item.name}
+                              className="object-cover"
+                            />
+                          </div>
+                          <div className="flex flex-col gap-2 justify-between w-full">
+                            <div>
+                              <p className="line-clamp-2 wrap-break-words font-semibold">
+                                {item.name}
+                              </p>
+                              <p className="text-neutral-400">
+                                RM
+                                {(item.currentPrice ?? item.unitPrice) *
+                                  item.quantity}
+                              </p>
+                            </div>
+                            <Stepper
+                              value={item.quantity}
+                              min={1}
+                              onChange={(setNewQuantity) =>
+                                useCart
+                                  .getState()
+                                  .setQuantity(item.id, setNewQuantity)
+                              }
+                            />
+                          </div>
+                        </div>
+                        <button
+                          className="p-3 hidden sm:block shrink-0 cursor-pointer rounded-full text-red-500 hover:bg-red-100"
+                          onClick={() => useCart.getState().clearItem(item.id)}
+                        >
+                          <Trash className="w-6 h-6" />
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                  <div className="fixed bottom-0 left-0 right-0 z-50 p-4 sm:p-8 shadow-md border-t border-neutral-200 bg-white">
+                    <div className="flex w-full items-center justify-between">
+                      <div className="flex gap-2 text-xl text-neutral-500">
+                        Total: <span>RM{totalCart.toFixed(2)}</span>
+                      </div>
+                      <Link
+                        href="/checkout"
+                        onClick={() => {
+                          useCheckout
+                            .getState()
+                            .setCheckoutData(cartItems, totalCart);
+                          setOpenCart(false);
+                        }}
+                        className="block p-4 rounded-lg overflow-hidden cursor-pointer border border-violet-600 text-white bg-violet-600 hover:text-violet-600 hover:bg-white"
                       >
-                        <Trash className="w-6 h-6" />
-                      </button>
+                        Checkout
+                      </Link>
                     </div>
-                  </div>
-                ))}
-                <div className="fixed bottom-0 left-0 right-0 z-50 p-4 sm:p-8 shadow-md border-t border-neutral-200 bg-white">
-                  <div className="flex w-full items-center justify-between">
-                    <div className="flex gap-2 text-xl text-neutral-500">
-                      Total: <span>RM{totalCart.toFixed(2)}</span>
-                    </div>
-                    <Link
-                      href="/checkout"
-                      onClick={() => {
-                        useCheckout
-                          .getState()
-                          .setCheckoutData(cartItems, totalCart);
-                        setOpenCart(false);
-                      }}
-                      className="block p-4 rounded-lg overflow-hidden cursor-pointer border border-violet-600 text-white bg-violet-600 hover:text-violet-600 hover:bg-white"
-                    >
-                      Checkout
-                    </Link>
                   </div>
                 </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
-        </div>
-      )}
-    </nav>
+        )}
+      </nav>
+    </header>
   );
 }
